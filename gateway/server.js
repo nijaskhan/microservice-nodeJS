96553839df18637962a35b2545fd29da6620e73a
nijaskhan <nijaskhan93@gmail.com>
2023-06-16 11:38:53 +0530

    docker setup fully completed

--- gateway/server.js
+++ gateway/server.js
@@ -8,12 +8,10 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-// app.use('/', (req,res)=>{
-//     res.status(200).json({message: 'home page'})
-// });  
-app.use('/customers', proxy('http://localhost:1234'));
-app.use('/admin', proxy('http://localhost:5000'));
-app.use('/', proxy('http://localhost:1234'));
+// this proxy is set up for docker conatiner so run this service inside a docker
+app.use('/customers', proxy('http://microservicecustomer:1234'));
+app.use('/admin', proxy('http://microserviceadmin:5000'));
+app.use('/', proxy('http://microservicecustomer:1234'));
 
 app.listen(port ,() => {
     console.log(`Server running on port ${port}`);
